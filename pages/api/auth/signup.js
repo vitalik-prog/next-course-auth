ad976ce7db51150c3f31020701d44d6bed1469f5
vitalii.varfolomieiev <vitalii.varfolomieiev@onix-systems.com>
2023-03-25 16:02:07 +0200

    added signup with email

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,35 +1,56 @@
+import {
+  collection,
+  where,
+  query,
+  addDoc,
+  getDocs,
+} from "firebase/firestore";
+import { db } from "../../../helpers/firebaseDb";
 import { hashPassword } from "../../../helpers/auth";
-import { connectToDatabase } from "../../../helpers/db";
 
-async function handler (req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, password } = req.body;
 
-    if (!email || !email.includes("@") || !password || password.trim().length < 7) {
-      res.status(422).json({ message: "Invalid input - password should be at least 7 characters long." });
+    if (!email || email.trim() === "") {
+      res.status(422).json({ message: "Invalid email address." });
       return;
     }
-    // get collection with name credentials from firestore
-    // const querySnapshot = await firestore.collection('credentials').get();
-    // get all documents from collection
-    // const documents = querySnapshot.docs;
 
-    // const client = await connectToDatabase();
-    // const db = client.db();
+    if (!email.trim().match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) {
+      res.status(422).json({ message: "Please enter a valid email" });
+      return;
+    }
 
-    // const isUserExist = await db.collection('users').findOne({ email });
-    // if (isUserExist) {
-    //   res.status(422).json({ message: "User exists already!" });
-    //   client.close();
-    //   return;
-    // }
+    if (!password || password.trim().length < 5) {
+      res.status(422).json({ message: "Password should be at least 5 characters" });
+      return;
+    }
 
-    // const hashedPassword = await hashPassword(password);
+    try {
+      const q = query(collection(db, "users"), where("email", "==", email));
 
-    // const result = await db.collection("users").insertOne({ email, password: hashedPassword });
-
-    res.status(201).json({ message: "Created user!" });
-    // client.close();
+      const querySnapshot = await getDocs(q);
+      const users = [];
+      querySnapshot.forEach((doc) => {
+        users.push(doc.data().email);
+      });
+  
+      if (users && users.length > 0) {
+        res.status(422).json({ message: "User exists already!" });
+        return;
+      }
+  
+      const result = await addDoc(collection(db, "users"), {
+        email,
+        password: await hashPassword(password),
+      });
+  
+      res.status(201).json({ message: "Created user!" });
+    } catch (error) {
+      res.status(500).json({ message: "Something went wrong" });
+      return;
+    }
   }
 }
 
